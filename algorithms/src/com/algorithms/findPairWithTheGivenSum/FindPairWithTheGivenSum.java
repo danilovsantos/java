3b7a0a1f22e84c60112b462a1ddef510ac79da47
danilo.valente <danilo.valente@fornecedores.sicoob.com.br>
2025-02-27 06:29:23 -0300

    Creating projects

---
+++ algorithms/src/com/algorithms/findPairWithTheGivenSum/FindPairWithTheGivenSum.java
@@ -0,0 +1,37 @@
+package com.algorithms.findPairWithTheGivenSum;
+
+public class FindPairWithTheGivenSum {
+    public static void main(String[] args) {
+
+        var reset = 0;
+        var count = 0;
+        var y = 0;
+        var target = 10;
+        int[] numbers = new int[]{8, 7, 2, 5, 3, 1};
+
+        for(int x = 0; x < numbers.length-1; x++){
+
+            var a = numbers[y];
+            var b = numbers[x+1];
+            count++;
+
+            if((a+b) == target){
+                System.out.println(a+"+"+b+"="+target);
+            }
+
+            if(x == numbers.length-2){
+                y++;
+                x = y;
+                reset++;
+
+            }
+
+        }
+
+        //System.out.println("");
+        System.out.println("iterations: "+count);
+        System.out.println("resets: "+reset);
+
+
+    }
+}
