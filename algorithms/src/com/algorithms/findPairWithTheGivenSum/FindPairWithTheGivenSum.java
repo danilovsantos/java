24002a3fba50e62bcc25b407f9d37f599756b549
danilo.valente <danilo.valente@fornecedores.sicoob.com.br>
2025-03-17 17:36:13 -0300

    improvements

--- algorithms/src/com/algorithms/findPairWithTheGivenSum/FindPairWithTheGivenSum.java
+++ algorithms/src/com/algorithms/findPairWithTheGivenSum/FindPairWithTheGivenSum.java
@@ -1,13 +1,22 @@
 package com.algorithms.findPairWithTheGivenSum;
 
+import java.util.Arrays;
+
 public class FindPairWithTheGivenSum {
+
+    private static int target = 10;
+    private static int[] numbers = new int[]{7, 2, 8, 1, 3, 5};
+
     public static void main(String[] args) {
+        findUsingBruteForce();
+        findUsingSorting();
+    }
+
+    public static void findUsingBruteForce(){
 
         var reset = 0;
         var count = 0;
         var y = 0;
-        var target = 10;
-        int[] numbers = new int[]{8, 7, 2, 5, 3, 1};
 
         for(int x = 0; x < numbers.length-1; x++){
 
@@ -16,21 +25,78 @@ public class FindPairWithTheGivenSum {
             count++;
 
             if((a+b) == target){
-                System.out.println(a+"+"+b+"="+target);
+                System.out.println(a+" + "+b+" = "+target);
             }
 
             if(x == numbers.length-2){
                 y++;
                 x = y;
                 reset++;
+            }
+
+        }
+
+        System.out.println("Iterations: "+count);
+        System.out.println("Reset Loops: "+reset);
+    }
+
+    public static void findUsingSorting(){
+
+        numbers = sortNumbers(numbers);
+
+        int left = 0;
+        int right = numbers.length-1;
+
+        for(int x = 0; x < numbers.length; x++){
+
+            int a = numbers[left];
+            int b = numbers[right];
+
+            if((a+b) == target){
+                System.out.println(a+" + "+b+" = "+target);
+                break;
+            }else if((a+b) > target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+
+    }
+
+    public static int[] sortNumbers(int[] numbers){
+
+        for(int x = 0; x < numbers.length-1; x++) {
+
+            //{7, 2, 8, 1, 3, 5};
+
+            int a = numbers[x];
+            int b = numbers[x+1];
+
+            if(a > b){
+                numbers[x] = b;
+                numbers[x+1] = a;
+            }
+
+            if(x > 0){
+
+                for(int y = x; y > 0; y--){
+
+                    int c = numbers[y];
+                    int d = numbers[y-1];
+                    if(c < d){
+                        numbers[y] = d;
+                        numbers[y-1] = c;
+                    }
+
+                }
 
             }
 
         }
-        
-        System.out.println("iterations: "+count);
-        System.out.println("resets: "+reset);
 
+        return numbers;
 
     }
+
 }
